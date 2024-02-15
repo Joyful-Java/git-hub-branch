89241e45eb4798925b5461477449adc858200776
Joyful-Java <nagaanish@gmail.com>
2024-02-15 12:09:22 +0530

    versio1 completed

---
+++ Test2V1.java
@@ -0,0 +1,9 @@
+class Test2V1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("Learn java");
+        System.out.println("Code in java");
+	}
+}
